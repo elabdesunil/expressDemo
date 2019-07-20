f276f85aaa7ed4a5d6baf73167c5b5cd9d435ee6
Frank <frankamalafronte@gmail.com>
2019-07-20 18:30:16 -0400

    first commit test env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+let key = process.env.React_APP_API_KEY
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App api ={key} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
